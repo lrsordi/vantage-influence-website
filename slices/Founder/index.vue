4de81f60abe46e21e9e02464bc229cd30f75705d
Marcelo Arocha <marceloarocha@gmail.com>
2026-05-26 22:36:15 -0300

    news adjusts

--- slices/Founder/index.vue
+++ slices/Founder/index.vue
@@ -15,9 +15,94 @@ defineProps(
 
 <template>
   <section
+    class="slice founder"
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    Placeholder component for founder (variation: {{ slice.variation }}) Slices
+    <div class="container">
+      <h2 v-if="slice.primary.section_title" class="section-title">
+        {{ slice.primary.section_title }}
+      </h2>
+      <div class="row align-items-center">
+        <div class="col-md-5">
+          <div class="founder-image">
+            <PrismicImage :field="slice.primary.image" />
+          </div>
+        </div>
+        <div class="col-md-7">
+          <div class="founder-info">
+            <p v-if="slice.primary.name" class="founder-name">{{ slice.primary.name }}</p>
+            <p v-if="slice.primary.role" class="founder-role">{{ slice.primary.role }}</p>
+            <div v-if="slice.primary.text" class="founder-text rich-text">
+              <PrismicRichText :field="slice.primary.text" />
+            </div>
+            <a v-if="slice.primary.email" :href="`mailto:${slice.primary.email}`" class="founder-email">
+              {{ slice.primary.email }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
+
+<style lang="scss">
+.founder {
+  padding-top: 10rem;
+  padding-bottom: 10rem;
+
+  .section-title {
+    @include slice-title;
+    margin-bottom: 6rem;
+  }
+
+  .founder-image {
+    img {
+      width: 100%;
+      height: auto;
+      object-fit: cover;
+    }
+  }
+
+  .founder-info {
+    padding-left: 4rem;
+
+    .founder-name {
+      font-family: $primary-font;
+      font-size: 2.5rem;
+      font-weight: 300;
+      margin-bottom: 0.5rem;
+    }
+
+    .founder-role {
+      font-family: $secondary-font;
+      font-size: 1.4rem;
+      font-weight: 400;
+      text-transform: uppercase;
+      letter-spacing: 0.1em;
+      margin-bottom: 3rem;
+    }
+
+    .founder-text {
+      font-family: $secondary-font;
+      font-size: 1.6rem;
+      font-weight: 300;
+      line-height: 1.6;
+      margin-bottom: 3rem;
+    }
+
+    .founder-email {
+      font-family: $secondary-font;
+      font-size: 1.4rem;
+      font-weight: 400;
+      text-decoration: none;
+      color: inherit;
+      border-bottom: 1px solid currentColor;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+  }
+}
+</style>
--- slices/News/index.vue
+++ slices/News/index.vue
@@ -29,12 +29,19 @@ const rootEl = ref<HTMLDivElement>();
     ref="rootEl"
   >
     <div class="container">
+      <h2 v-if="slice.primary.section_title" class="section-title">
+        {{ slice.primary.section_title }}
+      </h2>
       <div
         class="news-row"
         v-for="item in slice.items"
         :key="$prismic.asText(item.title)"
       >
         <NewsItem :item="item" :theme="slice.primary.theme" />
+      </div>
+
+      <div class="news-border-bottom" v-if="slice.primary.bottom_line">
+        <div class="horizontal-line top red"></div>
       </div>
     </div>
   </section>
@@ -45,8 +52,22 @@ const rootEl = ref<HTMLDivElement>();
   padding-top: 6rem;
   margin-top: 10rem;
 
+  .section-title {
+    font-family: $secondary-font;
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: $red;
+    margin-bottom: 4rem;
+    margin-top: 2rem;
+    text-transform: uppercase;
+  }
+
   .news-row {
     margin-bottom: 10rem;
+  }
+
+  .news-border-bottom {
+    position: relative;
   }
 }
 </style>
